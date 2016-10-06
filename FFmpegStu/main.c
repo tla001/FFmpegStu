c856a29be9fe7d0585746b8275bb50623f730dc6
tla001 <1414419047@qq.com>
2016-10-06 10:06:14 +0800

    build

---
+++ FFmpegStu/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: tla001
+ */
+
+
+/**
+ * @file
+ * libavcodec API use example.
+ *
+ * @example decoding_encoding.c
+ * Note that libavcodec only handles codecs (mpeg, mpeg4, etc...),
+ * not file formats (avi, vob, mp4, mov, mkv, mxf, flv, mpegts, mpegps, etc...). See library 'libavformat' for the
+ * format handling
+ */
+
+#include "CodeDemo.h"
+
+int main(int argc, char **argv){
+	//CodeDemo( argc, argv);
+	//SimpleDecoder();
+	//SDLDisplayTest1();
+	return 0;
+}
+
+
+
